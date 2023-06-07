282798a8ce4802188503cefdf730d3fb54c6cf96
Rodrigo-Hidalgo-Nicho <89040394+Rodrigo-Hidalgo-Nicho@users.noreply.github.com>
2023-06-07 18:50:43 -0500

    feat: added scripts

---
+++ script/script.js
@@ -0,0 +1,7 @@
+/* ------ typing animation ------ */
+var typed = new Typed(".typing",{
+    strings:["","option to get","Web Developer", "Programmer" ],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
