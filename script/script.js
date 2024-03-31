798211ad7a58a61e67b2f8f60db4b764d139d606
Rodrigo-Hidalgo-Nicho <89040394+Rodrigo-Hidalgo-Nicho@users.noreply.github.com>
2024-03-31 13:06:50 -0500

    feat: Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,24 @@
-/* ------ typing animation ------ */
 var typed = new Typed(".typing",{
-    strings:["","option to get","Web Developer", "Programmer" ],
+    strings:["","option to get","uniforms", "suppliers" ],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
-})
+});
+
+function handleScroll() {
+    var aboutSection = document.querySelector('.about');
+    var aboutPosition = aboutSection.getBoundingClientRect().top;
+    var screenHeight = window.innerHeight;
+
+    if (aboutPosition < screenHeight / 2 && aboutPosition > -screenHeight / 2) {
+        aboutSection.classList.add('active');
+        var aboutImage = document.querySelector('.about-image');
+        var aboutText = document.querySelector('.about-text');
+        aboutImage.classList.add('active');
+        aboutText.classList.add('active');
+    } 
+}
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('load', handleScroll);
+
